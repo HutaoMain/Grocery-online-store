df09fdc363754d7bbe303feb47689e80237853c6
HutaoMain <alimohamedalcantara@gmail.com>
2023-04-13 22:52:12 +0800

    react multi carousel

--- online-grocery-store-client/src/App.tsx
+++ online-grocery-store-client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import ProfileMenu from "./components/profile-dropdown/ProfileMenu";
 import Topbar from "./components/topbar/Topbar";
+import "react-multi-carousel/lib/styles.css";
 
 function App() {
   return (
--- online-grocery-store-client/src/components/category/Category.tsx
+++ online-grocery-store-client/src/components/category/Category.tsx
@@ -4,31 +4,49 @@ import softdrinksImg from "../../assets/softdrinks.jpg";
 import snacksImg from "../../assets/snacks.png";
 import fruitsImg from "../../assets/fruits.jpg";
 import vegetablesImg from "../../assets/vegetables.jpg";
+import Carousel from "react-multi-carousel";
 
-interface CategoryType {
-  name: string;
-  image: string;
-}
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const Category = () => {
-  const categories: CategoryType[] = [
+  const categories = [
     { name: "Soft Drinks", image: softdrinksImg },
     { name: "Snacks", image: snacksImg },
     { name: "Fruits", image: fruitsImg },
     { name: "Vegetables", image: vegetablesImg },
   ];
+
   return (
-    <div>
-      <h1>Category</h1>
-      <div className="category-mapping">
-        {categories.map((category) => (
-          <div key={category.name} className="category">
-            <img src={category.image} alt={category.name} />
-            <div className="category-name">{category.name}</div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Carousel responsive={responsive} containerClass="carousel-container">
+      {categories.map((category) => (
+        <div key={category.name} className="category">
+          <img
+            className="category-image"
+            src={category.image}
+            alt={category.name}
+          />
+          <div className="category-name">{category.name}</div>
+        </div>
+      ))}
+    </Carousel>
   );
 };
 
